738732d1c7c1f3cd806748ff8ce41a8c6374555b
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-03-13 12:32:11 -0600

    fixed credential/connection issue

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,19 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+
+const sequelize = new Sequelize('ecommerce_db', 'root', 'Pokemon23?', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
 
 module.exports = sequelize;
